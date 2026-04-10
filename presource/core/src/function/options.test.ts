4c775c8482f995a25f1cd4c192f8471019b4a342
NQQT <devilz.angelz@gmail.com>
2026-04-11 01:06:22 +1000

    Added options for functions

---
+++ presource/core/src/function/options.test.ts
@@ -0,0 +1,23 @@
+import { functionOptions } from '@presource/core';
+
+describe('functionOptions requirements', () => {
+    // TS7022: example implicitly has type any because it does not have a type annotation and is referenced directly or indirectly in its own initializer.
+    const example = functionOptions({
+        get: async () => 'hello',
+        // TS7023: world implicitly has return type any because it does not have a return type annotation and is referenced directly or indirectly in one of its return expressions.
+        world: async () => {
+            // TS7022: value implicitly has type any because it does not have a type annotation and is referenced directly or indirectly in its own initializer.
+            const value = await example.get();
+            return `${value} world`;
+        }
+    });
+
+    beforeEach(() => {
+        example().reset();
+    });
+
+    it('should be able to execute methods correctly', async () => {
+        expect(await example.get()).toBe('hello');
+        expect(await example.world()).toBe('hello world');
+    });
+});
